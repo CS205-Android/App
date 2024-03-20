8e17f431a795dae40a791b904ccc0266acc8cb98
Bk49 <ericngyongwei@gmail.com>
2024-03-20 16:04:29 +0800

    Complete Top Bar for the application

--- app/src/main/java/mobile/cs205/MainActivity.kt
+++ app/src/main/java/mobile/cs205/MainActivity.kt
@@ -3,13 +3,22 @@ package mobile.cs205
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import mobile.cs205.composables.common.navigation.TopBar
 import mobile.cs205.ui.theme.CS205Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +26,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CS205Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                HomePage()
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun HomePage() {
+    Scaffold(
+        topBar = { TopBar("Hello World") },
+        bottomBar = {},
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { /*TODO*/ },
+                icon = { Icon(Icons.Outlined.PlayArrow, "Extended Floating Action Button") },
+                text = { Text(text = "Start a random Quiz!") }
+            )
+        }, floatingActionButtonPosition = FabPosition.Center
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                color = MaterialTheme.colorScheme.primary,
+                text = """
+                            This ia an example of a scaffold.
+                        """.trimIndent()
+            )
+        }
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomePagePreview() {
     CS205Theme {
-        Greeting("Android")
+        HomePage()
     }
 }
---
+++ app/src/main/java/mobile/cs205/composables/common/navigation/TopBar.kt
@@ -0,0 +1,50 @@
+package mobile.cs205.composables.common.navigation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import mobile.cs205.R
+import mobile.cs205.ui.theme.CS205Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(title: String = "Hist-O-SG") {
+    CenterAlignedTopAppBar(
+        modifier = Modifier.background(MaterialTheme.colorScheme.surface),
+        title = { Text(title, maxLines = 1, overflow = TextOverflow.Ellipsis) },
+        scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(
+            rememberTopAppBarState()
+        ),
+        actions = {
+            Icon(
+                painterResource(id = R.drawable.logo_only_invis),
+                contentDescription = "App logo",
+                tint = Color.Unspecified,
+                modifier = Modifier.height(56.dp)
+            )
+        }
+    )
+
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_5)
+@Composable
+fun TopBarPreview() {
+    CS205Theme {
+        TopBar()
+    }
+}

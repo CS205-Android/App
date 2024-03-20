8b916432be9af20ad6467993ade17510079b96b8
Ng Wei Yang, Nigel <nigelng26@gmail.com>
2024-03-21 00:53:12 +0800

    feat: Add Dialog box which opens based on corresponding quiz

--- app/src/main/java/mobile/cs205/QuizListingActivity.kt
+++ app/src/main/java/mobile/cs205/QuizListingActivity.kt
@@ -8,14 +8,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import mobile.cs205.ui.theme.CS205Theme
 
@@ -59,6 +66,10 @@ class QuizListingActivity : ComponentActivity() {
                         "person 10", "person 11", "person 12",
                         "person 10", "person 11", "person 12",
             )
+//      State to keep track of which person is opened and when its opened. Will change with actual quiz
+        val openAlertDialog = remember { mutableStateOf(false) }
+        val (selectedItem, setSelectedItem) = remember { mutableStateOf("") }
+
         Scaffold (
 
         ) {
@@ -66,21 +77,37 @@ class QuizListingActivity : ComponentActivity() {
             LazyColumn {
                 items(names) { name ->
                     TextButton(
-                        onClick = { onClick() },
+                        onClick = {
+//                            Sets state to name of selected item and AlertDialog to true
+                            setSelectedItem(name)
+                            openAlertDialog.value = true
+                        },
                         modifier = Modifier.fillMaxWidth()) {
                         Text(name, modifier = Modifier.padding(innerPadding))
                     }
                     Divider()
                 }
             }
+//            When dialog state = true, set corresponding values based on AlertDialogExample Composable
+            when {
+                openAlertDialog.value -> {
+                    AlertDialogExample(
+                        onDismissRequest = { openAlertDialog.value = false },
+                        onConfirmation = {
+                            openAlertDialog.value = false
+                            println("Confirmation registered for $selectedItem")
+                        },
+                        dialogTitle = "Alert dialog example",
+                        dialogText = "You selected: $selectedItem",
+                        icon = Icons.Default.Info
+                    )
+
+                }
+            }
         }
 
     }
 
-    private fun onClick() {
-//        TODO("Not yet implemented")
-        println("Button pressed")
-    }
 
     @Preview(showSystemUi = true, showBackground = true)
     @Composable
@@ -88,5 +115,47 @@ class QuizListingActivity : ComponentActivity() {
         CS205Theme {
             ShowList()
         }
+    }
+
+    @Composable
+    fun AlertDialogExample(
+        onDismissRequest: () -> Unit,
+        onConfirmation: () -> Unit,
+        dialogTitle: String,
+        dialogText: String,
+        icon: ImageVector,
+    ) {
+        AlertDialog(
+            icon = {
+                Icon(icon, contentDescription = "Example Icon")
+            },
+            title = {
+                Text(text = dialogTitle)
+            },
+            text = {
+                Text(text = dialogText)
+            },
+            onDismissRequest = {
+                onDismissRequest()
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmation()
+                    }
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                    }
+                ) {
+                    Text("Dismiss")
+                }
+            }
+        )
     }
 }

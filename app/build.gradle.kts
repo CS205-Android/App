761ae7af50b6cc5b04dd30963ee77143f2637779
Bk49 <ericngyongwei@gmail.com>
2024-03-23 16:33:08 +0800

    Import and use navigation for Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.core.splashscreen)
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }

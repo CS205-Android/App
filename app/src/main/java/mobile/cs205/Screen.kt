c26846f913fc3ec5b88be2815a3b2d94f9d22be5
Bk49 <ericngyongwei@gmail.com>
2024-03-23 16:33:45 +0800

    Create sealed Screen class to store information of various screens

---
+++ app/src/main/java/mobile/cs205/Screen.kt
@@ -0,0 +1,9 @@
+package mobile.cs205
+
+sealed class Screen(val route: String) {
+    data object Home : Screen("home")
+    data object QuizListing : Screen("quiz_listing")
+    data object Question : Screen("question")
+    data object Settings : Screen("setting")
+    data object Scaffold : Screen("scaffold")
+}

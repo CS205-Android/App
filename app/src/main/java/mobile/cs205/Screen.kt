1eb974a1fd032f03ccea01385e82582e152ae4fe
Bk49 <ericngyongwei@gmail.com>
2024-03-27 12:40:44 +0800

    Remove unused Question screen

--- app/src/main/java/mobile/cs205/Screen.kt
+++ app/src/main/java/mobile/cs205/Screen.kt
@@ -3,7 +3,6 @@ package mobile.cs205
 sealed class Screen(val route: String) {
     data object Home : Screen("home")
     data object QuizListing : Screen("quiz_listing")
-    data object Question : Screen("question")
     data object Settings : Screen("setting")
     data object Scaffold : Screen("scaffold")
     data object QuizQuestion: Screen("quiz_question")

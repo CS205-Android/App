c9e8ab8c266711861d590dad2cfc8ca8f7ebaa99
Bk49 <ericngyongwei@gmail.com>
2024-03-27 12:37:23 +0800

    Fix TriviaCard not able to render images properly

--- app/src/main/java/mobile/cs205/composables/home/card/TriviaCard.kt
+++ app/src/main/java/mobile/cs205/composables/home/card/TriviaCard.kt
@@ -5,24 +5,30 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import coil.request.ImageRequest
 import mobile.cs205.data.remote.HistoryService
 import mobile.cs205.data.remote.SingaporeImageListing
 import mobile.cs205.data.remote.data.HistoryResponse
@@ -32,6 +38,10 @@ import mobile.cs205.ui.theme.CS205Theme
 fun TriviaCard() {
     val config = LocalConfiguration.current
     val screenWidth = config.screenWidthDp.dp
+    val imageModel = ImageRequest.Builder(LocalContext.current)
+        .data(SingaporeImageListing[SingaporeImageListing.indices.random()])
+        .crossfade(true)
+        .build()
     val service = HistoryService.create()
     val history = produceState(
         initialValue = HistoryResponse(
@@ -44,33 +54,38 @@ fun TriviaCard() {
         val data = service.getHistory()
         value = data[data.indices.random()]
     }
-    val image = produceState(initialValue = "") {
-        value = SingaporeImageListing[SingaporeImageListing.indices.random()]
-    }
 
     ElevatedCard(
-        modifier = androidx.compose.ui.Modifier
+        modifier = Modifier
             .width(screenWidth - 48.dp)
             .height(IntrinsicSize.Min)
             .wrapContentHeight()
     ) {
-        Box(modifier = with(Modifier) {
-            fillMaxWidth()
-                .height(screenWidth / 1.8f)
-                .background(Color.Blue)
-        }) {
-            AsyncImage(
+        Box(
+            modifier = with(Modifier) {
+                fillMaxWidth()
+                    .height(screenWidth / 1.8f)
+                    .background(MaterialTheme.colorScheme.surface)
+            },
+        ) {
+            SubcomposeAsyncImage(
                 contentScale = ContentScale.FillBounds,
-                modifier = with(Modifier) {
-                    fillMaxWidth()
-                        .height(screenWidth / 1.8f)
-                },
-                model = image.value,
+                model = imageModel,
                 contentDescription = "Singapore"
-            )
+            ) {
+                val state = painter.state
+                if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                        CircularProgressIndicator(modifier = Modifier.size(30.dp))
+                    }
+                } else {
+                    SubcomposeAsyncImageContent()
+                }
+            }
         }
 
         Column(modifier = Modifier.padding(24.dp)) {
+
             Text(text = "Do You know?", style = MaterialTheme.typography.bodyLarge)
             Text(
                 text = "In ${history.value.day}/${history.value.month}/${history.value.year}",

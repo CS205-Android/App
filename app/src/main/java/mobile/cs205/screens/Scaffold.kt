94e143b0fc71d272d481668a280bd18e6f62d2d3
Bk49 <ericngyongwei@gmail.com>
2024-03-27 12:39:07 +0800

    Remove StartQuizFAB

--- app/src/main/java/mobile/cs205/screens/Scaffold.kt
+++ app/src/main/java/mobile/cs205/screens/Scaffold.kt
@@ -2,8 +2,6 @@ package mobile.cs205.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -12,8 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import mobile.cs205.FragmentNavGraph
-import mobile.cs205.Screen
-import mobile.cs205.composables.common.button.StartQuizFAB
 import mobile.cs205.composables.common.navigation.BottomBar
 import mobile.cs205.composables.common.navigation.TopBar
 
@@ -27,12 +23,6 @@ fun ScaffoldScreen(rootNavController: NavHostController) {
     Scaffold(
         topBar = { TopBar(current) },
         bottomBar = { BottomBar(navController, setCurrent) },
-        floatingActionButtonPosition = FabPosition.Center,
-        floatingActionButton = {
-            if (current == "Hist-O-SG") {
-                StartQuizFAB(rootNavController)
-            }
-        }
     ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
             FragmentNavGraph(navController = navController, rootNavController = rootNavController)

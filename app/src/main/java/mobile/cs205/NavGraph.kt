b29a745a95e1852c81f1cf67ee4f7d7b4e29df36
Bk49 <ericngyongwei@gmail.com>
2024-03-27 11:36:42 +0800

    Remove navController passing for HomeScreen in the NavGraph

--- app/src/main/java/mobile/cs205/NavGraph.kt
+++ app/src/main/java/mobile/cs205/NavGraph.kt
@@ -5,7 +5,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import mobile.cs205.screens.HomeScreen
-import mobile.cs205.screens.QuestionScreen
 import mobile.cs205.screens.QuizListingScreen
 import mobile.cs205.screens.QuizQuestionScreen
 import mobile.cs205.screens.ScaffoldScreen
@@ -22,7 +21,7 @@ fun MainNavGraph(navController: NavHostController) {
 @Composable
 fun FragmentNavGraph(navController: NavHostController, rootNavController: NavHostController){
     NavHost(navController = navController, startDestination = Screen.Home.route) {
-        composable(route = Screen.Home.route) { HomeScreen(rootNavController) }
+        composable(route = Screen.Home.route) { HomeScreen() }
         composable(route = Screen.QuizListing.route) { QuizListingScreen(rootNavController) }
         composable(route = Screen.Settings.route) { SettingsScreen()}
     }
